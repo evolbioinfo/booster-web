9945cbfbd943532369546066829328b91021d58c
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2017-02-17 10:42:40 +0100

    Changed itol uploader

--- server/handlefuncs.go
+++ server/handlefuncs.go
@@ -1,18 +1,16 @@
 package server
 
 import (
-	"archive/zip"
 	"bufio"
-	"bytes"
 	"compress/gzip"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"mime/multipart"
 	"net/http"
 	"regexp"
 	"strings"
 
+	"github.com/fredericlemoine/gotree/upload"
 	"github.com/fredericlemoine/sbsweb/io"
 )
 
@@ -55,86 +53,15 @@ func itolHandler(w http.ResponseWriter, r *http.Request, id string) {
 		errorHandler(w, r, existerr)
 		return
 	}
-	hc := http.Client{}
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-	part, err := writer.CreateFormFile("zipFile", "tree.tree.zip")
+	upld := upload.NewItolUploader("", "")
+	url, _, err := upld.Upload(fmt.Sprintf("%d", a.Id), a.result)
 	if err != nil {
 		io.LogError(err)
 		errorHandler(w, r, err)
 		return
 	}
-	b, comperr := compressString("tree.tree", a.result.Newick())
-	if comperr != nil {
-		io.LogError(comperr)
-		errorHandler(w, r, comperr)
-		return
-	}
 
-	part.Write(b)
-	_ = writer.WriteField("treeFormat", "newick")
-	_ = writer.WriteField("treeName", a.Id)
-
-	err = writer.Close()
-	if err != nil {
-		io.LogError(err)
-		errorHandler(w, r, err)
-		return
-	}
-	req, err2 := http.NewRequest("POST", "http://itol.embl.de/batch_uploader.cgi", body)
-	if err2 != nil {
-		io.LogError(err2)
-		errorHandler(w, r, err2)
-		return
-	}
-	//req.Header.Set("Content-Type", writer.FormDataContentType())
-	req.Header.Set("Content-Type", "multipart/form-data")
-	resp, err3 := hc.Do(req)
-	if err3 != nil {
-		io.LogError(err3)
-		errorHandler(w, r, err3)
-		return
-	}
-
-	defer resp.Body.Close()
-	bodyresp, err4 := ioutil.ReadAll(resp.Body)
-	if err4 != nil {
-		io.LogError(err4)
-		errorHandler(w, r, err4)
-		return
-	}
-	stringresp := string(bodyresp)
-	infos := strings.Split(stringresp, "\n")
-	errregexp, regerr := regexp.Compile("^ERR")
-	if regerr != nil {
-		io.LogError(regerr)
-		errorHandler(w, r, regerr)
-		return
-	}
-	succregexp, regerr2 := regexp.Compile("^SUCCESS: (\\S+)")
-	if regerr2 != nil {
-		io.LogError(regerr2)
-		errorHandler(w, r, regerr2)
-		return
-	}
-
-	if errregexp.MatchString(infos[len(infos)-1]) {
-		io.LogError(errors.New(fmt.Sprintf("Upload failed. iTOL returned the following error message: %s", infos[len(infos)-1])))
-		errorHandler(w, r, err4)
-		return
-	}
-
-	sub := succregexp.FindStringSubmatch(infos[len(infos)-1])
-	if len(sub) < 2 {
-		sub = succregexp.FindStringSubmatch(infos[0])
-	}
-
-	if len(sub) > 1 {
-		http.Redirect(w, r, "http://itol.embl.de/tree/"+sub[1], http.StatusSeeOther)
-	} else {
-		w.Header().Set("Content-Type", "text/plain")
-		w.Write(bodyresp)
-	}
+	http.Redirect(w, r, url, http.StatusSeeOther)
 }
 
 func newHandler(w http.ResponseWriter, r *http.Request) {
@@ -214,24 +141,4 @@ func GetFormFileReader(f multipart.File, h *multipart.FileHeader) (*bufio.Reader
 		reader = bufio.NewReader(f)
 	}
 	return reader, nil
-}
-
-func compressString(filename, s string) ([]byte, error) {
-	var b bytes.Buffer
-	zw := zip.NewWriter(&b)
-	z, e := zw.Create(filename)
-	if e != nil {
-		return b.Bytes(), e
-	}
-	if _, err := z.Write([]byte(s)); err != nil {
-		return b.Bytes(), err
-	}
-
-	if err := zw.Flush(); err != nil {
-		return b.Bytes(), err
-	}
-	if err := zw.Close(); err != nil {
-		return b.Bytes(), err
-	}
-	return b.Bytes(), nil
 }
