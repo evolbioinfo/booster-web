d74c1f86719a8cc4e751b4356d48676eed31a0d2
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2017-02-24 11:51:22 +0100

    Handle several signals

--- server/server.go
+++ server/server.go
@@ -27,6 +27,7 @@ import (
 	"path/filepath"
 	"runtime"
 	"sync"
+	"syscall"
 	"time"
 
 	"github.com/fredericlemoine/booster-web/config"
@@ -278,7 +279,12 @@ func initUUIDGenerator() {
 func initCleanKill() {
 
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Interrupt,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+
 	go func() {
 		for sig := range c {
 			log.Print(sig)
