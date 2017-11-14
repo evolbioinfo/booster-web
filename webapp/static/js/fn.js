6fe6261d1c4d28ea101100522fc8b2ab627e0b51
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2017-11-14 16:00:18 +0100

    Dynamic Range input + Clear files + form style

--- webapp/static/js/fn.js
+++ webapp/static/js/fn.js
@@ -1,3 +1,18 @@
 function updateNbootInput(val) {
     document.getElementById('nboottext').innerHTML=val; 
 }
+
+$( document ).ready(function() {
+    // When sequence file is selected,
+    // Tree files are cleared
+    $("#refseqs").change(function (){
+	$("#reftree").val('');
+	$("#boottrees").val('');
+    });
+    $("#reftree").change(function (){
+	$("#refseqs").val('');
+    });
+    $("#boottrees").change(function (){
+	$("#refseqs").val('');
+    });
+});
