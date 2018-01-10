21354ac8d821c33e29ef7c6d1a16033ee53778e7
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2018-01-10 15:43:03 +0100

    Changed ref

--- notification/notification.go
+++ notification/notification.go
@@ -71,7 +71,7 @@ func (n *NullNotifier) Notify(status string, analysisId string, workflow string,
 func (n *EmailNotifier) Notify(status string, analysisId string, workflow string, email string) (err error) {
 	// Connect to the remote SMTP server.
 	if email != "" && n.server != "" && n.user != "" && n.pass != "" && n.sender != "" && validateEmail(email) {
-		ref := "Lemoine, F., Entfellner, J. B. D., Wilkinson, E., De Oliveira, T., & Gascuel, O. (2017). Boosting Felsenstein Phylogenetic Bootstrap. bioRxiv."
+		ref := "Lemoine, F., Entfellner, J. B. D., Wilkinson, E., Correia, D., Davila Felipe, M., De Oliveira, T., & Gascuel, O. (2017). Boosting Felsenstein Phylogenetic Bootstrap. bioRxiv."
 		jobstr := "Booster[1]"
 		if workflow == "PhyML-SMS" {
 			ref = "[1] Lefort, V., Longueville, J. E., & Gascuel, O. (2017). SMS: Smart Model Selection in PhyML. Molecular Biology and Evolution.\n[2] " + ref
